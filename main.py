3bc8b4bc1bbabfc94a75ead6cb4e7cc52c6281a4
Noordeen Malango <noordinmalango@gmail.com>
2020-01-10 22:16:23 +0200

    converting text to lower and removing numbers

---
+++ main.py
@@ -0,0 +1,10 @@
+#converting your text to lowercase
+input_str = "The 5 biggest countries by population in 2017 are China, India, United States, Indonesia, and Brazil."
+input_str = input_str.lower()
+#print(input_str)
+
+#Removing numbers if they are not necessary in your analysis
+import re
+input_str = 'Box A contains 3 red and 5 white balls, while Box B contains 4 red and 2 blue balls.'
+result = re.sub(r'\d+', '', input_str)
+print(result)
